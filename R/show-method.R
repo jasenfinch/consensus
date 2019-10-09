daeb6afefabb76ad5eb9c3431fc316f7819b6579
jasenfinch <jsf9@aber.ac.uk>
2019-10-09 21:47:13 +0100

    added to Consensus show method

--- R/show-method.R
+++ R/show-method.R
@@ -1,5 +1,15 @@
 
 setMethod('show','Consensus',
           function(object){
-  
-})
+            cat('Consensus structural classifications\n\n')
+            cat('MF:\t\t',mf(object),'\n')
+            cat('Adducts:\t',nrow(adductRules(object)),'\n')
+            cat('Organism:\t',organism(object),'\n')
+            cat('Database:\t',database(object),'\n')
+            cat('Threshold:\t',str_c(threshold(object),'%'),'\n')
+            cat('Hits:',nrow(hits(object)),'\n')
+            cat('PIPs:',nrow(PIPs(object)),'\n')
+            cat('Classifications:',nrow(classifications(object)),'\n')
+            cat('Consensuses:',nrow(consensusClassifications(object)))
+          }
+)
