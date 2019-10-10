9471512737bb8220a5790f3dadf46b6f84290bb4
jasenfinch <jsf9@aber.ac.uk>
2019-10-10 11:25:02 +0100

    removed Consensuses class

--- R/allClasses.R
+++ R/allClasses.R
@@ -25,16 +25,3 @@ setClass('Consensus',
            consensus = 'tbl_df'
          )
 )
-
-#' Consensuses
-#' @description An S4 class to store consensus structural classifications.
-#' @slot consensuses list containing consensus results for each MF
-#' @slot results tibble containing consensus structural classifications for each feature
-#' @export
-
-setClass('Consensuses',
-         slots = list(
-           consensuses = 'list',
-           results = 'tbl_df'
-         )
-)
