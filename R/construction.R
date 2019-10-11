4424c0bf244d4220e19c89a8a436cc42b0761787
jasenfinch <jsf9@aber.ac.uk>
2019-10-11 10:16:46 +0100

    added construction documentation

--- R/construction.R
+++ R/construction.R
@@ -1,6 +1,14 @@
 
 globalVariables(c('Compound','Consensus (%)','Enzyme','InChI','SMILES','ID','Level','Label','INCHIKEY'))
 
+#' construction
+#' @description Build or add to a consensus classification library. 
+#' @param MFs vector of molecular formulas
+#' @param path target file path for classification library 
+#' @param db databases to search. Can be either kegg or pubchem.
+#' @param organism KEGG organism ID. Ignored if kegg is not specified in db.
+#' @param adductRules adduct rules table as returned by mzAnnotation::adducts()
+#' @param threshold \% majority threshold for consensus classifications 
 #' @export
 
 construction <- function(MFs, path = '.', db = c('kegg','pubchem'), organism = character(), adductRules = adducts(), threshold = 50){
