68aba5b563e3f06179688cd7bf76534291b69c42
jasenfinch <jsf9@aber.ac.uk>
2022-06-21 19:39:30 +0100

    fix construction unit tests

--- tests/testthat/test_construction.R
+++ tests/testthat/test_construction.R
@@ -2,15 +2,12 @@
 context('construction')
 
 test_that('construction workds',{
-  a <- tibble(MF = c('C4H11N','C10H14O7P2'),Adduct = rep('[M-H]1-',2))  
-  res <- construction(a,path = tempdir())
-  res1 <- construction(a,path = tempdir())
+  MFs <- tibble(MF = c('C4H11N','C10H14O7P2'),
+              Adduct = rep('[M-H]1-',2))
   
-  l <- loadLibrary(a,tempdir())
-  
-  p <- capture.output(print(l[[1]]))
+  res <- construction(MFs)
+  res1 <- construction(MFs)
   
   expect_equal(nrow(res),2)
   expect_equal(nrow(res1),2)
-  expect_length(p,13)
 })
