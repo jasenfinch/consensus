9f726e1fb956a0542ddfbc77ffb86bdde060698a
jasenfinch <jsf9@aber.ac.uk>
2022-06-22 13:44:46 +0100

    remove export of Consensus class

--- R/consensus.R
+++ R/consensus.R
@@ -1,14 +1,3 @@
-#' Consensus
-#' @description An S4 class to store consensus structral classification results for a molecular formula.
-#' @slot MF molecular formula
-#' @slot adduct_rules tibble containing adduct rules as returned by \code{mzAnnotation::adducts()}
-#' @slot organism organism KEGG ID. NA if database is pubchem.
-#' @slot database database, kegg or pubchem
-#' @slot threshold \% majority for consensus classification
-#' @slot classifications structural classifications for database hits
-#' @slot PIPs putative ionisation product matches from database hits for given adduct rules
-#' @slot consensus consensus structural classifications for adducts
-#' @export
 
 setClass('Consensus',
          slots = list(
